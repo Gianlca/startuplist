b2913ff9b9d963dfd880f16f84d3a76cfc300bd8
gianluca.cina <gianluca.cina@eng.it>
2020-05-12 14:37:56 +0200

    api change

--- src/API.js
+++ src/API.js
@@ -1,4 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080' : null;
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://https://startuplist.now.sh';
 
 export let listLogEntries = async() => {
     const response = await fetch(`${API_URL}/organizations.json`);
