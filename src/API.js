5b6dcdec8457fa5d856094f76a9fae89d36a0723
gianluca.cina <gianluca.cina@eng.it>
2020-05-12 15:00:43 +0200

    public folder

--- src/API.js
+++ src/API.js
@@ -1,6 +1,4 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://startuplist.now.sh';
-
 export let listLogEntries = async() => {
-    const response = await fetch(`${API_URL}/organizations.json`);
+    const response = await fetch(`public/data/organizations.json`);
     return response.json();
 }
